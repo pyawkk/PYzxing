3c98c6607a1d09a2a12f12287c234e71468301a3
pyawkk <624204727@qq.com>
2015-03-27 18:13:36 +0800

    删除修改多余的代码

--- PYzxing/ViewController.m
+++ PYzxing/ViewController.m
@@ -34,64 +34,18 @@
     [button3 setFrame:CGRectMake(50, 300, 140, 50)];
     [button3 addTarget:self action:@selector(pressButton3:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:button3];
-    
-//    qrReader = [[NSMutableSet alloc ] init];
-//    ZXQRCodeReader* qrcodeReader = [[ZXQRCodeReader alloc] init];
-//    [qrReader addObject:qrcodeReader];
-//    scanningQR = NO;
-
-//    self.step = STEP_QR;
-
 }
-
-// AVFoundation的回调函数
-//- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputSampleBuffer:(CMSampleBufferRef)sampleBuffer fromConnection:(AVCaptureConnection *)connection {
-//    // 第一步，将sampleBuffer转成UIImage
-//    UIImage *image= [self getCaptureImage:sampleBuffer];
-//    // 第二步，用Decoder识别图象
-//    Decoder *d = [[Decoder alloc] init];
-//    d.readers = self.qrReader;
-//    d.delegate = self;
-//    self.scanningQR = [d decodeImage:image] == YES ? NO : YES;
-//}
-
-
-- (void)decodeImage:(UIImage *)image
-{
-    NSMutableSet *qrReader = [[NSMutableSet alloc] init];
-        ZXQRCodeReader *qrcoderReader = [[ZXQRCodeReader alloc] init];
-        [qrReader addObject:qrcoderReader];
-    
-
-    
-        ZXAztecDecoder *decoder = [[ZXAztecDecoder alloc] init];
-    
-    
-    
-    
-    
-    
-//        decoder.delegate = self;
-//        decoder.readers = qrReader;
-//        [decoder decodeImage:image];
-}
-
 
 -(void)getURLWithImage:(UIImage *)img{
     
     UIImage *loadImage= img;
     CGImageRef imageToDecode = loadImage.CGImage;
     
-    
-    // Given a CGImage in which we are looking for barcodes
-    
     ZXLuminanceSource *source = [[ZXCGImageLuminanceSource alloc] initWithCGImage:imageToDecode];
     ZXBinaryBitmap *bitmap = [ZXBinaryBitmap binaryBitmapWithBinarizer:[ZXHybridBinarizer binarizerWithSource:source]];
     
     NSError *error = nil;
     
-    // There are a number of hints we can give to the reader, including
-    // possible formats, allowed lengths, and the string encoding.
     ZXDecodeHints *hints = [ZXDecodeHints hints];
     
     ZXMultiFormatReader *reader = [ZXMultiFormatReader reader];
@@ -103,18 +57,13 @@
         // result.rawBytes and result.length.
         NSString *contents = result.text;
         NSLog(@"contents =%@",contents);
-        UIAlertView *alter = [[UIAlertView alloc] initWithTitle:@"成功" message:contents delegate:nil cancelButtonTitle:@"确定" otherButtonTitles: nil];
+        UIAlertView *alter = [[UIAlertView alloc] initWithTitle:@"解析成功" message:contents delegate:nil cancelButtonTitle:@"确定" otherButtonTitles: nil];
         [alter show];
-        // The barcode format, such as a QR code or UPC-A
-        //        ZXBarcodeFormat format = result.barcodeFormat;
-    } else {
-        NSLog(@"解析错误");
-        // Use error to determine why we didn't get a result, such as a barcode
-        // not being found, an invalid checksum, or a format inconsistency.
-    }
 
-    
-    
+    } else {
+        UIAlertView *alter1 = [[UIAlertView alloc] initWithTitle:@"解析失败" message:nil delegate:nil cancelButtonTitle:@"确定" otherButtonTitles: nil];
+        [alter1 show];
+    }
 }
 
 
@@ -125,16 +74,11 @@
      UIImage *loadImage= self.myPIC.image;
     CGImageRef imageToDecode = loadImage.CGImage;
     
-
-    // Given a CGImage in which we are looking for barcodes
-    
     ZXLuminanceSource *source = [[ZXCGImageLuminanceSource alloc] initWithCGImage:imageToDecode];
     ZXBinaryBitmap *bitmap = [ZXBinaryBitmap binaryBitmapWithBinarizer:[ZXHybridBinarizer binarizerWithSource:source]];
     
     NSError *error = nil;
     
-    // There are a number of hints we can give to the reader, including
-    // possible formats, allowed lengths, and the string encoding.
     ZXDecodeHints *hints = [ZXDecodeHints hints];
     
     ZXMultiFormatReader *reader = [ZXMultiFormatReader reader];
@@ -142,19 +86,16 @@
                                 hints:hints
                                 error:&error];
     if (result) {
-        // The coded result as a string. The raw data can be accessed with
-        // result.rawBytes and result.length.
         NSString *contents = result.text;
         NSLog(@"contents =%@",contents);
-        // The barcode format, such as a QR code or UPC-A
-//        ZXBarcodeFormat format = result.barcodeFormat;
+        UIAlertView *alter = [[UIAlertView alloc] initWithTitle:@"解析地址" message:contents delegate:nil cancelButtonTitle:@"确定" otherButtonTitles: nil];
+        [alter show];
     } else {
-        NSLog(@"失败");
-        // Use error to determine why we didn't get a result, such as a barcode
-        // not being found, an invalid checksum, or a format inconsistency.
+        UIAlertView *alter = [[UIAlertView alloc] initWithTitle:@"解析失败" message:nil delegate:nil cancelButtonTitle:@"确定" otherButtonTitles: nil];
+        [alter show];
     }
-    
 }
+
 
 - (void)pressButton3:(UIButton *)button
 {
@@ -181,7 +122,6 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
